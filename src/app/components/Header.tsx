5aa82c96235b3ab16950bd281f5bfe003d1bf0d6
uttampun <uttampun50@gmail.com>
2024-08-22 09:07:58 +0545

    project setup

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div className="header bg-[#262845]">
+        <div className="headerContainer max-w-[1440px] mx-auto">
+           <div className="headerTitle">
+             <h1 className="text-white">Uttam</h1>
+           </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/app/types/Button.ts
@@ -0,0 +1,5 @@
+
+interface ButtonProps{
+    value?:string,
+    type?:string
+}
