8172d90a1a74caffd347c3517b38b73a93aa1724
uttampun <uttampun50@gmail.com>
2024-08-25 08:29:33 +0545

    speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
       <Header />
         {children}
+        <SpeedInsights />
         <Footer />
         </body>
     </html>
